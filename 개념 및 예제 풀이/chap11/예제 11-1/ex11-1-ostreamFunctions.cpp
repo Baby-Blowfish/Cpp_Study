a71d2c968d47ba2d52750428cd2e5538bbb04c46
Baby-Blowfish <roboticsdev.kim@gmail.com>
2024-08-01 09:48:02 +0900

    update

--- 개념 및 예제 풀이/chap11/예제 11-1/ex11-1-ostreamFunctions.cpp
+++ 개념 및 예제 풀이/chap11/예제 11-1/ex11-1-ostreamFunctions.cpp
@@ -1,3 +1,10 @@
+/*
+	stream(스트림) : 연속적인 데이터의 흐름을 혹은 데이터를 전송하는 소프트웨어 모듈
+					프로그램과 장치를 연결하며 바이트 단위로 입출력
+
+	input stream	: 키보드, 네트워크, 파일 등 입력장치로부터 입력된 데이터를 순서대로 프로그램에 전달하는 객체
+	output stream	: 
+*/
 #include <iostream>
 using namespace std;
 
